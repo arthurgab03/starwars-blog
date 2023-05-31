e08269110c272600a8b7e5c468913680b223b2fb
Arthur Gabriel <arthurgabriel120703@gmail.com>
2023-05-31 14:28:30 -0300

    first commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+      "id":"1",
+      "title": "O RETORNO DE THRAWN",
+      "description": "Grande Almirante Thrawn, personagem conhecido e amado por fãs vai retornar na série live-action Ahsoka™.",
+      "photoCover":"https://static1.srcdn.com/wordpress/wp-content/uploads/2020/11/The-Mandalorian-Ahsoka-Grand-Admiral-Thrawn.jpg"
+    },
+    {
+      "id":"2",
+      "title": "LORDE VADER EM ANDOR",
+      "description": "É possível que o vilão Darth Vader apareça na última temporada da grandiosa série Andor™.",
+      "photoCover":"https://images.thedirect.com/media/article_full/andor-vader.jpg"
+    },
+    {
+        "id":"3",
+        "title": "The Mandalorian Season 4",
+        "description": "A tão amada série The Mandalorian™ está com sua quarta temporada em fase de produção!",
+        "photoCover":"https://uploads.metropoles.com/wp-content/uploads/2020/09/08130252/the-mandalorian-season-2.jpg"
+      },
+      {
+        "id":"4",
+        "title": "JEDI: SURVIVOR É UM SUCESSO!",
+        "description": "O jogo protagonizado por Cal Kestis trouxe diversas novidades e planetas maiores para explorar, Jedi: Survivor está mais grandioso do que nunca!",
+        "photoCover":"https://cabanadoleitor.com.br/wp-content/uploads/2023/05/Star-Wars-Jedi-Survivor-1280x742.webp"
+      }
+  ]
